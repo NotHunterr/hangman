850e44bd0384766ed385fa8ab1d8049f1f1caa95
Hunter <notryoshi@gmail.com>
2025-10-22 14:12:06 -0400

    Base game done, lots of bug fixes to do!

--- main.go
+++ main.go
@@ -3,12 +3,15 @@
 // BE ABLE TO SEE YOUR PREVIOUS GUESSES
 // DETERMINE CHOSEN WORD AND PRELOAD ALL CHARACTER SPACES BEFORE FOR LOOP
 //DISPLAY CURRENT SCORE E.X: H_APP_ (HAPPY)
+// MAKE SURE YOU CANT GUESS THE SAME THING AGAIN
+// CHECK FOR TWO OF SAME LETTER IN WORD.
 
 package main
 
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -31,23 +34,47 @@ func main() {
 
 	randIndex := rand.Intn(len(randWords))
 
-	chosenWord := randWords[randIndex]
+	secretWord := randWords[randIndex]
 	fmt.Println(startGameASCII)
 	fmt.Println("Welcome to Hangman! Your only goal is to guess the random word that the computer has chosen!")
+	fmt.Println(secretWord)
 	var won bool = false
 	var letter string
 	var misses int = 0
+	var totalRight int = 0
 
+	// Game Loop
 	for !won {
 		fmt.Println("Please take your guess now!")
 		fmt.Scanln(&letter)
 
-		if !strings.Contains(chosenWord, letter) {
+		if !strings.Contains(secretWord, letter) {
 			misses += 1
-		} else {
+			fmt.Printf("I'm sorry, the letter %s is not in the word!", letter)
 
-			//iterate over chosen word with selected letter to find the spot that it is in
-			fmt.Printf("The letter %s is in the secret phrase! It is in the %s spot", letter)
+		} else {
+			// Inits length of secret word, the creates a loop and iterates over each letter,
+			// checking if it is the same as the one guessed
+			wordLen := len(secretWord)
+			i := 0
+
+			for i < wordLen {
+				slice := secretWord[i:(i + 1)]
+				if slice != letter {
+					i += 1
+				} else {
+					fmt.Printf("You are correct! The letter %s is in the %s spot\n", letter, strconv.Itoa(i+1))
+					totalRight += 1
+
+					if totalRight == wordLen {
+						fmt.Printf("Congratulations, you have solved the word! The word was %s", secretWord)
+						won = true
+						break
+					}
+					break
+				}
+
+			}
 		}
 
 		switch misses {
@@ -63,7 +90,12 @@ func main() {
 			fmt.Println(fiveL)
 		case 6:
 			fmt.Println(sixL)
-			fmt.Printf("You have lost I'm so very sorry! The correct word was %s", chosenWord)
+			fmt.Printf("You have lost I'm so very sorry! The correct word was %s", secretWord)
+
+		}
+		// If you have missed 6 letters, the game is over
+		if misses == 6 {
+			break
 		}
 
 	}
